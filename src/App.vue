b424086bb46503c2dbf9ba403553da4675dc1c0e
Roberto Butti <roberto.butti@gmail.com>
2018-10-19 21:29:14 +0200

    First commit with: - Vue.js - HERE XYZ maps - Stamen tiles

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!--img alt="Vue logo" src="./assets/logo.png"-->
+    <XyzMap msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import XyzMap from './components/XyzMap.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    XyzMap
   }
 }
 </script>
 
 <style>
+body {
+  margin: 0px;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 0px;
 }
 </style>
---
+++ src/components/XyzMap.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="map" ref="myMap">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'XyzMap',
+  data: {
+      map: null
+  },
+  props: {
+    msg: String
+  },
+  mounted: function () {
+    // `this` points to the vm instance
+    console.log('a is: ' + this.msg)
+    //specify your credentials for image and link layers
+	var YOUR_ACCESS_TOKEN = "";
+    console.log('access: ' + YOUR_ACCESS_TOKEN)
+	// configure layers
+    var layers = [
+        new here.xyz.maps.layers.TileLayer({
+            name: 'Image Layer',
+            min: 1,
+            max: 20,
+            provider: new here.xyz.maps.providers.ImageProvider({
+                name: 'Live Map',
+                //url : 'https://{SUBDOMAIN_INT_1_4}.mapcreator.tilehub.api.here.com/tilehub/wv_livemap_bc/png/sat/256/{QUADKEY}?access_token='+YOUR_ACCESS_TOKEN
+                url: 'http://tile.stamen.com/watercolor/{z}/{x}/{y}.jpg'
+            })
+        })
+    ]
+    console.log('layers created')
+    console.log(this.$refs)
+	// setup the Map Display
+    this.map = new  here.xyz.maps.Map( this.$refs.myMap, {
+        zoomLevel : 17,
+        center: {
+            longitude: -122.46852, latitude: 37.77956
+        },
+        // add layers to display
+        layers: layers
+    });
+    console.log('map created')
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.map {
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100vh;
+}
+
+
+</style>
