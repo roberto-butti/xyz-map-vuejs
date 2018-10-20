1910b3b11dea8cfdff2c0dbe4f3e8f26048b9593
Roberto Butti <roberto.butti@gmail.com>
2018-10-20 16:05:51 +0200

    Adding Space and Space Provider

--- src/components/XyzMap.vue
+++ src/components/XyzMap.vue
@@ -16,14 +16,17 @@ export default {
     // `this` points to the vm instance
     console.log('a is: ' + this.msg)
     //specify your credentials for image and link layers
-	var YOUR_ACCESS_TOKEN = "";
+	var YOUR_ACCESS_TOKEN = process.env.VUE_APP_SPACE_TOKEN; //readonly token
     console.log('access: ' + YOUR_ACCESS_TOKEN)
+
+    // Define provider for this layer
 	// configure layers
     var layers = [
         new here.xyz.maps.layers.TileLayer({
             name: 'Image Layer',
             min: 1,
             max: 20,
+            
             provider: new here.xyz.maps.providers.ImageProvider({
                 name: 'Live Map',
                 //url : 'https://{SUBDOMAIN_INT_1_4}.mapcreator.tilehub.api.here.com/tilehub/wv_livemap_bc/png/sat/256/{QUADKEY}?access_token='+YOUR_ACCESS_TOKEN
@@ -44,6 +47,29 @@ export default {
         layers: layers
     });
     console.log('map created')
+
+    //ADDING SPACES
+
+    var mySpaceProvider = new here.xyz.maps.providers.SpaceProvider ({
+        name:  process.env.VUE_APP_SPACE,
+        level: 2,
+        space: process.env.VUE_APP_SPACE,
+        credentials: {
+                access_token: YOUR_ACCESS_TOKEN
+        }
+    });
+    console.log('space provider', mySpaceProvider)
+    // Create data layer with Space provider
+    var mySpaceLayer = new here.xyz.maps.layers.TileLayer({
+    name: 'mySpaceLayer',
+    min: 2,
+    max: 20,
+    provider: mySpaceProvider
+    })
+    console.log("spacelayer", mySpaceLayer)
+    this.map.addLayer( mySpaceLayer )
+    console.log("spacelayer added", this.map)
+
   }
 }
 </script>
